bdbbf5996da5930a0410aae4f60132fdaa7ef995
Mohamedbachir120 <mohamedbhadjadji@gmail.com>
2023-02-25 07:55:51 +0100

    add user

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const app = express()
+
+const server = require('http').createServer(app)
+
+
+const io = require('socket.io')(server,{
+    cors:{origin:'http://localhost:5173'}
+})
+
+io.on('connection',(socket)=>{
+    console.log('connection');
+    socket.on('sendNotificationToUser',(obj)=> {
+        console.log(obj);
+        socket.broadcast.emit('receiveNotificationToUser',obj.message)
+    })
+    socket.on('disconnect',(socket)=>{
+
+    })
+})
+
+const port = 3000
+
+server.listen(port, ()=>{
+        console.log('server is running on port'+port)
+})
